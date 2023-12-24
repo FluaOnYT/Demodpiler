85c9075e7cb9e30af66bb25a8fa8c00f81e96330
FluaOnYT <124430414+FluaOnYT@users.noreply.github.com>
2023-12-24 20:55:17 +0800

    Updated main

--- source/main.lua
+++ source/main.lua
@@ -1,10 +1,28 @@
 local GET = loadstring and function(...) return game:HttpGet(...) end or function(...) return game:GetService("HttpService"):GetAsync(...) end
 
 local Extractors = {
-  String = GET("https://cdn.jsdelivr.net/gh/FluaOnYT/Demodpiler@main/source/extractors/string.lua"),
-  Number = GET("https://cdn.jsdelivr.net/gh/FluaOnYT/Demodpiler@main/source/extractors/number.lua"),
+  string = GET("https://cdn.jsdelivr.net/gh/FluaOnYT/Demodpiler@main/source/extractors/string.lua"),
+  number = GET("https://cdn.jsdelivr.net/gh/FluaOnYT/Demodpiler@main/source/extractors/number.lua"),
 }
 
-function Extract(Module)
+function _Extract(Table, Indent)
+  local Extracted = "return {"
+
+  for I, V in next, Table do
+    if type(V) == "table" then
+      Extracted = Extracted .. _Extract(V, Indent + 1)
+    else
+      local Extrator = Extractors[type(V)]
+      
+      Extracted = Extracted .. Extractor(V, Indent, I)
+    end
+  end
   
+  return Extracted
+end
+
+function Extract(Module)
+  Module = require(Module)
+
+  return _Extract(Module)
 end
