f09989570f58b2fc8f1ee53154c3107ee910f667
FluaOnYT <124430414+FluaOnYT@users.noreply.github.com>
2023-12-31 20:31:18 +0800

    Update main
    Included support of more types
    
    Fixed SharedTables.new
    
    Fixed an issue

--- source/main.lua
+++ source/main.lua
@@ -13,6 +13,13 @@ local Extractors = {
 
 		return Line
 	end,
+	boolean = function(Value: boolean)
+		local Line = ""
+		
+		Line = Value and "true" or "false"
+		
+		return Line
+	end,
 	Instance = function(Value: Instance)
 		local Line = ""
 		local Parents = {Value.Name}
@@ -113,28 +120,67 @@ local Extractors = {
 	end,
 	RaycastParams = function(Value: RaycastParams)
 		local Line = "RaycastParams.new()"
-		
+
 		return Line
 	end,
 	Ray = function(Value: Ray)
 		local Line = ""
-		
+
 		Line = string.format("Ray.new(Vector3.new(%f, %f, %f), Vector3.new(%f, %f, %f))", Value.Origin.X, Value.Origin.Y, Value.Origin.Z, Value.Direction.X, Value.Direction.Y, Value.Direction.Z)
-		
+
 		return Line
 	end,
 	PhysicalProperties = function(Value: PhysicalProperties)
 		local Line = ""
-		
+
 		Line = string.format("PhysicalProperties.new(%f, %f, %f, %f, %f)", Value.Density, Value.Friction, Value.Elasticity, Value.FrictionWeight, Value.ElasticityWeight)
+
+		return Line
+	end,
+	PathWaypoint = function(Value: PathWaypoint)
+		local Line = ""
+
+		Line = string.format("PathWaypoint.new(Vector3.new(%f, %f, %f), Enum.PathWaypointAction.%s, %s)", Value.Position.X, Value.Position.Y, Value.Position.Z, Value.Action.Name, Value.Label)
+
+		return Line
+	end,
+	NumberSequenceKeypoint = function(Value: NumberSequenceKeypoint)
+		local Line = ""
+
+		Line = string.format("NumberSequenceKeypoint.new(%f, %f, %f)", Value.Time, Value.Value, Value.Envelope)
+
+		return Line
+	end,
+	NumberRange = function(Value: NumberRange)
+		local Line = ""
+		
+		Line = string.format("NumberRange.new(%f, %f)", Value.Min, Value.Max)
+		
+		return Line
+	end,
+	Color3 = function(Value: Color3)
+		local Line = ""
+		
+		Line = string.format("Color3.fromRGB(%f, %f, %f)", Value.R, Value.G, Value.B)
 		
 		return Line
 	end,
 }
 
-function _Extract(Table, Indent)
-	local Extracted = "{\n"
+function SharedTabletoTable(Table)
+	local t = {}
+	
+	for i, v in Table do
+		t[i] = v
+	end
+	
+	return t
+end
 
+function _Extract(Table: SharedTable | {any}, Indent)
+	local Extracted = "{\n"
+	local Table = typeof(Table) == "SharedTable" and SharedTabletoTable(Table) or Table
+	
 	for I, V in pairs(Table) do
 		local Line = ""
 
@@ -159,7 +205,7 @@ function _Extract(Table, Indent)
 			local Extractor = Extractors[typeof(V)]
 
 			if Extractor == nil then
-				Extracted = Extracted .. Line .. string.format("nil, --[[%s type is not supported]]", typeof(V))
+				Extracted = Extracted .. Line .. string.format("nil --[[%s type is not supported]]", typeof(V))
 			else
 				Extracted = Extracted .. Line .. Extractor(V)
 			end
@@ -173,6 +219,8 @@ function _Extract(Table, Indent)
 			Extracted = Extracted .. "  "
 		end
 	end
+
+	Extracted = Extracted:gsub(".000000", "")
 
 	return Extracted .. "}"
 end
