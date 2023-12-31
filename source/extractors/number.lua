d4d37c3cfed18433b14ca3763d3c4187efa2af23
FluaOnYT <124430414+FluaOnYT@users.noreply.github.com>
2023-12-31 15:21:31 +0800

    Delete source/extractors directory

--- source/extractors/number.lua
+++
@@ -1,7 +0,0 @@
-return function(Value, Key)
-  local Line = ""
-  
-  Line = Line .. string.format("%d", tonumber(Value))
-
-  return Line .. ",\n"
-end
--- source/extractors/string.lua
+++
@@ -1,7 +0,0 @@
-return function(Value: string)
-		local Line = ""
-
-		Line = Line .. string.format("\"%s\"", tostring(Value))
-
-		return Line
-end
