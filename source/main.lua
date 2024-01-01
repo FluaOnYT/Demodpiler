d1fcfbb0db665044e69a214efefe173be81d9e8a
FluaOnYT <124430414+FluaOnYT@users.noreply.github.com>
2024-01-01 11:24:41 +0800

    Update main

--- source/main.lua
+++ source/main.lua
@@ -1,179 +1,252 @@
-local Extractors = {
-	string = function(Value: string)
-		local Line = ""
+local Extractors = {}
 
-		Line = Line .. string.format("\"%s\"", tostring(Value))
+Extractors.string = function(Value: string)
+	local Line = ""
 
-		return Line
-	end,
-	number = function(Value: number)
-		local Line = ""
+	Line = Line .. string.format("\"%s\"", tostring(Value))
 
-		Line = Line .. string.format("%f", Value)
+	return Line
+end
+Extractors.number = function(Value: number)
+	local Line = ""
 
-		return Line
-	end,
-	boolean = function(Value: boolean)
-		local Line = ""
-		
-		Line = Value and "true" or "false"
-		
-		return Line
-	end,
-	Instance = function(Value: Instance)
-		local Line = ""
-		local Parents = {Value.Name}
-		local PResult = {}
-		local Parent = Value
+	Line = Line .. string.format("%f", Value)
 
-		if Parent.Parent == nil then
-			Line = "Instance.new(\"" .. Value.ClassName .. "\")"
-		else
-			while Parent ~= game do
-				if Parent.Parent == game then
-					table.insert(Parents, "game")
-				else
-					table.insert(Parents, Parent.Parent.Name)
-				end
-
-				Parent = Parent.Parent
-			end
-
-			table.freeze(Parents)
-
-			for I = 1, #Parents do
-				PResult[I] = Parents[#Parents - I + 1]
-			end
-
-			Line = Line .. table.concat(PResult, ".")
-		end
-
-		return Line
-	end,
-	Vector2 = function(Value: Vector2)
-		local Line = ""
-
-		Line = string.format("Vector2.new(%f, %f)", Value.X, Value.Y)
-
-		return Line
-	end,
-	Vector2int16 = function(Value: Vector2int16)
-		local Line = ""
-
-		Line = string.format("Vector2.new(%d, %d)", Value.X, Value.Y)
-
-		return Line
-	end,
-	Vector3 = function(Value: Vector3)
-		local Line = ""
-
-		Line = string.format("Vector3.new(%f, %f, %f)", Value.X, Value.Y, Value.Z)
-
-		return Line
-	end,
-	Vector3int16 = function(Value: Vector3int16)
-		local Line = ""
-
-		Line = string.format("Vector3.new(%d, %d, %d)", Value.X, Value.Y, Value.Z)
-
-		return Line
-	end,
-	CFrame = function(Value: CFrame)
-		local Line = ""
-		local Components = table.pack(Value:GetComponents())
-
-		if Components[4] == 1 and Components[8] == 1 and Components[12] == 1 then
-			Line = string.format("CFrame.new(%f, %f, %f)", Components[1], Components[2], Components[3])
-		else
-			Line = string.format("CFrame.new(%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f)", unpack(Components))
-		end
-
-		return Line
-	end,
-	UDim = function(Value: UDim)
-		local Line = ""
-
-		Line = string.format("UDim.new(%f, %f)", Value.Scale, Value.Offset)
-
-		return Line
-	end,
-	UDim2 = function(Value: UDim2)
-		local Line = ""
-
-		Line = string.format("UDim2.new(%f, %f, %f, %f)", Value.X.Scale, Value.X.Offset, Value.Y.Scale, Value.Y.Offset)
-
-		return Line
-	end,
-	Region3int16 = function(Value: Region3int16)
-		local Line = ""
-
-		Line = string.format("Region3int16.new(Vector3int16.new(%d, %d, %d), Vector3int16.new(%d, %d, %d))", Value.Min.X, Value.Min.Y, Value.Min.Z, Value.Max.X, Value.Max.Y, Value.Max.Z)
-
-		return Line
-	end,
-	TweenInfo = function(Value: TweenInfo)
-		local Line = ""
-
-		Line = string.format("TweenInfo.new(%f, Enum.EasingStyle.%s, Enum.EasingDirection.%s, %d, %s, %d)", Value.Time, Value.EasingStyle.Name, Value.EasingDirection.Name, Value.RepeatCount, Value.Reverses and "true" or "false", Value.DelayTime)
-
-		return Line
-	end,
-	RaycastParams = function(Value: RaycastParams)
-		local Line = "RaycastParams.new()"
-
-		return Line
-	end,
-	Ray = function(Value: Ray)
-		local Line = ""
-
-		Line = string.format("Ray.new(Vector3.new(%f, %f, %f), Vector3.new(%f, %f, %f))", Value.Origin.X, Value.Origin.Y, Value.Origin.Z, Value.Direction.X, Value.Direction.Y, Value.Direction.Z)
-
-		return Line
-	end,
-	PhysicalProperties = function(Value: PhysicalProperties)
-		local Line = ""
-
-		Line = string.format("PhysicalProperties.new(%f, %f, %f, %f, %f)", Value.Density, Value.Friction, Value.Elasticity, Value.FrictionWeight, Value.ElasticityWeight)
-
-		return Line
-	end,
-	PathWaypoint = function(Value: PathWaypoint)
-		local Line = ""
-
-		Line = string.format("PathWaypoint.new(Vector3.new(%f, %f, %f), Enum.PathWaypointAction.%s, %s)", Value.Position.X, Value.Position.Y, Value.Position.Z, Value.Action.Name, Value.Label)
-
-		return Line
-	end,
-	NumberSequenceKeypoint = function(Value: NumberSequenceKeypoint)
-		local Line = ""
-
-		Line = string.format("NumberSequenceKeypoint.new(%f, %f, %f)", Value.Time, Value.Value, Value.Envelope)
-
-		return Line
-	end,
-	NumberRange = function(Value: NumberRange)
-		local Line = ""
-		
-		Line = string.format("NumberRange.new(%f, %f)", Value.Min, Value.Max)
-		
-		return Line
-	end,
-	Color3 = function(Value: Color3)
-		local Line = ""
-		
-		Line = string.format("Color3.fromRGB(%f, %f, %f)", Value.R, Value.G, Value.B)
-		
-		return Line
-	end,
-	FloatCurveKey = function(Value: FloatCurveKey)
-		local Line = ""
-		
-		Line = string.format("FloatCurveKey.new(%d, %d, Enum.KeyInterpolationMode.%s)", Value.Time, Value.Value, Value.Interpolation.Name)
-		
-		return Line
-	end,
+	return Line
+end
+Extractors.boolean = function(Value: boolean)
+	local Line = ""
 	
-}
+	Line = Value and "true" or "false"
+	
+	return Line
+end
+Extractors.Instance = function(Value: Instance)
+	local Line = ""
+	local Parents = {Value.Name}
+	local PResult = {}
+	local Parent = Value
+
+	if Parent.Parent == nil then
+		Line = "Instance.new(\"" .. Value.ClassName .. "\")"
+	else
+		while Parent ~= game do
+			if Parent.Parent == game then
+				table.insert(Parents, "game")
+			else
+				table.insert(Parents, Parent.Parent.Name)
+			end
+
+			Parent = Parent.Parent
+		end
+
+		table.freeze(Parents)
+
+		for I = 1, #Parents do
+			PResult[I] = Parents[#Parents - I + 1]
+		end
+
+		Line = Line .. table.concat(PResult, ".")
+	end
+
+	return Line
+end
+Extractors.Vector2 = function(Value: Vector2)
+	local Line = ""
+
+	Line = string.format("Vector2.new(%f, %f)", Value.X, Value.Y)
+
+	return Line
+end
+Extractors.Vector2int16 = function(Value: Vector2int16)
+	local Line = ""
+
+	Line = string.format("Vector2.new(%d, %d)", Value.X, Value.Y)
+
+	return Line
+end
+Extractors.Vector3 = function(Value: Vector3)
+	local Line = ""
+
+	Line = string.format("Vector3.new(%f, %f, %f)", Value.X, Value.Y, Value.Z)
+
+	return Line
+end
+Extractors.Vector3int16 = function(Value: Vector3int16)
+	local Line = ""
+
+	Line = string.format("Vector3.new(%d, %d, %d)", Value.X, Value.Y, Value.Z)
+
+	return Line
+end
+Extractors.CFrame = function(Value: CFrame)
+	local Line = ""
+	local Components = table.pack(Value:GetComponents())
+
+	if Components[4] == 1 and Components[8] == 1 and Components[12] == 1 then
+		Line = string.format("CFrame.new(%f, %f, %f)", Components[1], Components[2], Components[3])
+	else
+		Line = string.format("CFrame.new(%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f)", unpack(Components))
+	end
+
+	return Line
+end
+Extractors.UDim = function(Value: UDim)
+	local Line = ""
+
+	Line = string.format("UDim.new(%f, %f)", Value.Scale, Value.Offset)
+
+	return Line
+end
+Extractors.UDim2 = function(Value: UDim2)
+	local Line = ""
+
+	Line = string.format("UDim2.new(%f, %f, %f, %f)", Value.X.Scale, Value.X.Offset, Value.Y.Scale, Value.Y.Offset)
+
+	return Line
+end
+Extractors.Region3int16 = function(Value: Region3int16)
+	local Line = ""
+
+	Line = string.format("Region3int16.new(%s, %s)", Extractors.Vector3int16(Value.Min), Extractors.Vector3int16(Value.Max))
+
+	return Line
+end
+Extractors.TweenInfo = function(Value: TweenInfo)
+	local Line = ""
+
+	Line = string.format("TweenInfo.new(%f, Enum.EasingStyle.%s, Enum.EasingDirection.%s, %d, %s, %d)", Value.Time, Value.EasingStyle.Name, Value.EasingDirection.Name, Value.RepeatCount, Value.Reverses and "true" or "false", Value.DelayTime)
+
+	return Line
+end
+Extractors.RaycastParams = function(Value: RaycastParams)
+	local Line = "RaycastParams.new()"
+
+	return Line
+end
+Extractors.Ray = function(Value: Ray)
+	local Line = ""
+
+	Line = string.format("Ray.new(%s, %s)", Extractors.Vector3(Value.Origin), Extractors.Vector3(Value.Direction))
+
+	return Line
+end
+Extractors.PhysicalProperties = function(Value: PhysicalProperties)
+	local Line = ""
+
+	Line = string.format("PhysicalProperties.new(%f, %f, %f, %f, %f)", Value.Density, Value.Friction, Value.Elasticity, Value.FrictionWeight, Value.ElasticityWeight)
+
+	return Line
+end
+Extractors.PathWaypoint = function(Value: PathWaypoint)
+	local Line = ""
+
+	Line = string.format("PathWaypoint.new(Vector3.new(%f, %f, %f), Enum.PathWaypointAction.%s, %s)", Value.Position.X, Value.Position.Y, Value.Position.Z, Value.Action.Name, Value.Label)
+
+	return Line
+end
+Extractors.NumberSequenceKeypoint = function(Value: NumberSequenceKeypoint)
+	local Line = ""
+
+	Line = string.format("NumberSequenceKeypoint.new(%f, %f, %f)", Value.Time, Value.Value, Value.Envelope)
+
+	return Line
+end
+Extractors.NumberSequence = function(Value: NumberSequence)
+	local Line = ""
+
+	for i, v in pairs(Value.Keypoints) do
+		table.insert(Keypoints, Extractors.NumberSequenceKeypoint(v))
+	end
+
+	Line = string.format("NumberSequence.new({%s})", table.concat(Keypoints))
+
+	return Line
+end
+Extractors.NumberRange = function(Value: NumberRange)
+	local Line = ""
+	
+	Line = string.format("NumberRange.new(%f, %f)", Value.Min, Value.Max)
+	
+	return Line
+end
+Extractors.Color3 = function(Value: Color3)
+	local Line = ""
+	
+	Line = string.format("Color3.fromRGB(%f, %f, %f)", Value.R, Value.G, Value.B)
+	
+	return Line
+end
+Extractors.FloatCurveKey = function(Value: FloatCurveKey)
+	local Line = ""
+	
+	Line = string.format("FloatCurveKey.new(%d, %d, Enum.KeyInterpolationMode.%s)", Value.Time, Value.Value, Value.Interpolation.Name)
+	
+	return Line
+end
+Extractors.Font = function(Value: Font)
+	local Line = ""
+
+	Line = string.format("Font.new(%s, Enum.FontWeight.%s, Enum.FontStyle.%s)", Value.Family, Value.Weight.Name, Value.Style.Name)
+
+	return Line
+end
+Extractors.DateTime = function(Value: DateTime)
+	local Line = ""
+
+	Line = string.format("DateTime.fromUnixTimestampMillis(%d)", Value.UnixTimestampMillis)
+
+	return Line
+end
+Extractors.ColorSequenceKeypoint = function(Value: ColorSequenceKeypoint)
+	local Line = ""
+
+	Line = string.format("ColorSequenceKeypoint.new(%f, %s)", Value, Extractors.Color3(Value.Color3))
+
+	return Line
+end
+Extractors.ColorSequence = function(Value: ColorSequence)
+	local Line = ""
+	local Keypoints = {}
+
+	for i, v in pairs(Value.Keypoints) do
+		table.insert(Keypoints, Extractors.ColorSequenceKeypoint(v))
+	end
+
+	Line = string.format("ColorSequence.new({%s})", table.concat(Keypoints))
+
+	return Line
+end
+Extractors.BrickColor = function(Value: BrickColor)
+	local Line = ""
+
+	Line = string.format("BrickColor.new(%d, %d, %d) --[[%s]]", Value.r, Value.g, Value.b, Value.Name)
+
+	return Line
+end
+Extractors.Axes = function(Value: Axes)
+	local Line = ""
+	local Faces = {}
+
+	if Value.Top == true then
+		table.insert(Faces, "Enum.NormalId.Top")
+	elseif Value.Bottom == true then
+		table.insert(Faces, "Enum.NormalId.Bottom")
+	elseif Value.Left == true then
+		table.insert(Faces, "Enum.NormalId.Left")
+	elseif Value.Right == true then
+		table.insert(Faces, "Enum.NormalId.Right")
+	elseif Value.Front = true then
+		table.insert(Faces, "Enum.NormalId.Front")
+	elseif Value.Back == true then
+		table.insert(Faces, "Enum.NormalId.Back")
+	end
+
+	Line = string.format("Axes.new({%s})", table.concat(Faces))
+
+	return Line
+end
+
 
 function SharedTabletoTable(Table)
 	local t = {}
